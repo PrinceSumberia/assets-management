fb03c932d5ef98b85a04e3061081cdb9d6f06b7b
Prince <broughtmeup@gmail.com>
2018-11-07 19:51:30 +0530

    Added Dynamic QRCode Generation in Single PDF

--- app/src/main/java/com/prince/assetManagement/GenerateQR.java
+++ app/src/main/java/com/prince/assetManagement/GenerateQR.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,9 +31,7 @@ import net.glxn.qrgen.android.QRCode;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import static ai.api.android.AIDataService.TAG;
@@ -44,6 +44,12 @@ public class GenerateQR extends AppCompatActivity {
     StorageReference storageReference;
     ArrayList<String> qrcode_urls = new ArrayList<String>();
     String image_url;
+    //    int listSize;
+    List<String> list = new ArrayList<>();
+
+    int listSize;
+    String welcome;
+    Button next;
 
 
     @Override
@@ -57,7 +63,7 @@ public class GenerateQR extends AppCompatActivity {
         final String id = getIntent().getStringExtra("id");
         final String detectedObject = getIntent().getStringExtra("detectedObject");
         imageView = findViewById(R.id.image);
-
+        next = findViewById(R.id.next_ac);
 
 
         Log.e(TAG, "The parent document id is" + id);
@@ -68,64 +74,76 @@ public class GenerateQR extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    List<String> list = new ArrayList<>();
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         Log.e(TAG, "Document.getId" + document.getId());
                         list.add(document.getId());
                     }
-                    int listSize = list.size();
-                    Log.d(TAG, "onComplete: List is " + list.toString());
-                    Log.d(TAG, "onComplete: List is " + list);
-                    Log.d(TAG, "onComplete: first element is" + list.get(0));
-                    for (int i = 0; i < listSize; i++) {
-                        String text = list.get(i);
-                        final Paragraph p = new Paragraph();
-                        p.add("Hello World");
-                        try {
-                            Bitmap bitmap = QRCode.from(list.get(i)).bitmap();
-                            final StorageReference ref = storageReference.child("/qrcode/" + UUID.randomUUID().toString());
-                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                            byte[] data = baos.toByteArray();
-                            ref.putBytes(data)
-                                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                        @Override
-                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                                @Override
-                                                public void onSuccess(Uri uri) {
-                                                    Toast.makeText(getApplicationContext(), "Uploaded", Toast.LENGTH_SHORT).show();
-                                                    image_url = uri.toString();
-                                                    qrcode_urls.add(image_url);
-                                                    Log.e(TAG, "qrcode url is" + qrcode_urls.toString());
-                                                    Log.d(TAG, "onSuccess: url = " + uri.toString());
-                                                    Toast.makeText(GenerateQR.this, "Images are stored here: " + image_url, Toast.LENGTH_SHORT).show();
-                                                }
-                                            });
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(getApplicationContext(), "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                        }
-                                    })
-                                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                                        @Override
-                                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
-                                                    .getTotalByteCount());
-                                        }
-                                    });
-                            Log.d(TAG, "Inside the Welcome to the app" + qrcode_urls.toString());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                    listSize = list.size();
+                }
+                Log.d(TAG, "Checking is going on: " + listSize);
+                Log.d(TAG, "onCreate: " + list.toString());
+                for (int i = 0; i < listSize; i++) {
+                    String text = list.get(i);
+                    final Paragraph p = new Paragraph();
+                    p.add("Hello World");
+                    try {
+                        Bitmap bitmap = QRCode.from(list.get(i)).bitmap();
+                        final StorageReference ref = storageReference.child("/qrcode/" + UUID.randomUUID().toString());
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] data = baos.toByteArray();
+                        ref.putBytes(data)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                Toast.makeText(getApplicationContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                                                image_url = uri.toString();
+                                                qrcode_urls.add(image_url);
+                                                Log.d(TAG, "onSuccess: url = " + uri.toString());
+                                                Toast.makeText(GenerateQR.this, "Images are stored here: " + image_url, Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                        Log.e(TAG, "qrcode url is" + qrcode_urls.toString());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(), "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                                .getTotalByteCount());
+                                    }
+                                })
+                                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        welcome = qrcode_urls.toString();
+                                        Log.d(TAG, "inside on complete " + welcome);
+                                        Log.d(TAG, "this is inside the oncomplete" + qrcode_urls.toString());
+                                        Toast.makeText(GenerateQR.this, "Complete You Can Now Proceed!", Toast.LENGTH_SHORT).show();
+                                        next.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                Intent intent1 = new Intent(getApplicationContext(), GetPDF.class);
+                                                intent1.putStringArrayListExtra("qrcode_links", qrcode_urls);
+                                                startActivity(intent1);
+                                            }
+                                        });
+                                    }
+                                });
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    Log.d(TAG, "Welcome to the app" + qrcode_urls.toString());
                 }
             }
         });
-        Log.d(TAG, "onCreate: url srting " + qrcode_urls.toString());
     }
 }
--- app/src/main/java/com/prince/assetManagement/GetPDF.java
+++ app/src/main/java/com/prince/assetManagement/GetPDF.java
@@ -1,13 +1,83 @@
 package com.prince.assetManagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class GetPDF extends AppCompatActivity {
+    private static final String TAG = GetPDF.class.getName();
+    ArrayList<String> qr_urls = new ArrayList<>();
+    TextView textView;
+
+    public static PdfPCell createImageCell(String path)
+            throws DocumentException, IOException {
+        Image img = Image.getInstance(path);
+        PdfPCell cell = new PdfPCell(img, true);
+        return cell;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_pdf);
+        textView = findViewById(R.id.list);
+        new MyAsyncTask().execute();
+
+    }
+
+    private class MyAsyncTask extends AsyncTask {
+        @Override
+        protected String doInBackground(Object... arg0) {
+
+            ////Execute the network related option here
+            qr_urls = getIntent().getStringArrayListExtra("qrcode_links");
+            Log.e(TAG, "onCreate: " + qr_urls.get(0));
+            textView.setText(qr_urls.toString());
+            int size = qr_urls.size();
+            String directoryPath = android.os.Environment.getExternalStorageDirectory().toString();
+            Document document = new Document();
+            try {
+                PdfWriter.getInstance(document, new FileOutputStream(directoryPath + "/example.pdf"));
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            document.open();
+            Paragraph p = new Paragraph();
+            PdfPTable table = new PdfPTable(1);
+            for (int i = 0; i < size; i++) {
+                table.setWidthPercentage(100);
+                try {
+                    table.addCell(createImageCell(qr_urls.get(i)));
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                document.add(table);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+            document.close();
+            return null;
+        }
     }
 }
