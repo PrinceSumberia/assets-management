7f6ed2860ab3ed7817cd5c1b0bcb3d978f281636
Prince <broughtmeup@gmail.com>
2019-01-20 20:27:31 +0530

    Fixed Search By User Logic

--- app/src/main/java/com/prince/assetManagement/FragmentUser.java
+++ app/src/main/java/com/prince/assetManagement/FragmentUser.java
@@ -1,108 +1,103 @@
 package com.prince.assetManagement;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static android.support.constraint.Constraints.TAG;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentUser.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentUser#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentUser extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public FragmentUser() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentUser.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentUser newInstance(String param1, String param2) {
-        FragmentUser fragment = new FragmentUser();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button get_info;
+    EditText editText;
+    TextView textView, textView1;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        get_info = view.findViewById(R.id.get_info);
+        editText = view.findViewById(R.id.asset_user);
+        textView = view.findViewById(R.id.result);
+        textView1 = view.findViewById(R.id.result_title);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        get_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String issued_to = editText.getText().toString();
+                db.collection("users")
+                        .document(user_id)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    Log.e(TAG, "onComplete: loop is executing");
+                                    final DocumentSnapshot documentSnapshot = task.getResult();
+                                    Log.e(TAG, "onComplete: assets are " + documentSnapshot.get("assets").toString());
+                                    for (final Object assets : (ArrayList) documentSnapshot.get("assets")) {
+                                        Log.e(TAG, "onComplete: Loop is executing " + assets);
+                                        db.collection("users")
+                                                .document(user_id)
+                                                .collection(assets.toString())
+                                                .whereEqualTo("issued_to", issued_to)
+                                                .get()
+                                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            final ArrayList<String> list = new ArrayList<>();
+                                                            Log.e(TAG, "onComplete: task is successful");
+                                                            Log.e(TAG, "onComplete: executing" + task.getResult().getDocuments().toString());
+                                                            Log.e(TAG, "onComplete: " + task.getResult().getMetadata().toString());
+                                                            textView1.setText(assets.toString().toUpperCase());
+                                                            String result = "Quantity: " + task.getResult().size() + "\n";
+                                                            textView.setText(result);
+                                                            textView.append("Date Issued: \n");
+                                                            int count = 0;
+                                                            for (QueryDocumentSnapshot documentSnapshot1 : task.getResult()) {
+                                                                list.add(documentSnapshot1.getData().get("issued_date").toString());
+                                                                Log.e(TAG, "onComplete: document snapshot " + documentSnapshot1.getData().get("issued_date").toString());
+                                                            }
+                                                            Set<String> unique = new HashSet<String>(list);
+                                                            for (String key : unique) {
+                                                                String new_result = (Collections.frequency(list, key) + " on " + key);
+                                                                textView.append("   \u2022 " + new_result + "\n");
+                                                            }
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return view;
     }
 }
