84b4a4be874f481c11b8def6751f4e6572df531e
Prince <broughtmeup@gmail.com>
2019-01-20 18:58:26 +0530

    Fixed Search By Department Logic

--- app/src/main/java/com/prince/assetManagement/FragmentDepartment.java
+++ app/src/main/java/com/prince/assetManagement/FragmentDepartment.java
@@ -14,14 +14,11 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 
 public class FragmentDepartment extends Fragment {
@@ -44,34 +41,68 @@ public class FragmentDepartment extends Fragment {
         get_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final ArrayList<String> list = new ArrayList<>();
-                db.collection("users").document(user_id).collection("laptop").whereEqualTo("department", editText.getText().toString()).get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    int count = 0;
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        String category_name = document.getData().get("category").toString();
-                                        Log.d(TAG, "Here is the result: " + document.getId() + " => " + document.getData());
-//                                        textView.setText(document.getData().get("category").toString());
-                                        list.add(document.getData().get("category").toString());
-                                    }
-                                    Set<String> unique = new HashSet<String>(list);
-                                    for (String key : unique) {
-                                        String result = (key + ": " + Collections.frequency(list, key));
-                                        textView.append(result + "\n");
-                                    }
-//                                    String result = ":" + list.size();
-//                                    textView.append(result);
-                                } else {
-                                    Log.d(TAG, "Error getting documents: ", task.getException());
-                                }
-
+                final String department = editText.getText().toString();
+                db.collection("users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            Log.e(TAG, "onComplete: " + documentSnapshot.get("assets"));
+                            for (final Object assets : (ArrayList) documentSnapshot.get("assets")) {
+                                Log.e(TAG, "onComplete: Inside lopp is executing");
+                                db.collection("users")
+                                        .document(user_id)
+                                        .collection(assets.toString())
+                                        .whereEqualTo("department", department)
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                if (task.isSuccessful()){
+                                                    Log.e(TAG, "onComplete: asset type is " + assets  +  "Number of assets" + task.getResult().size());
+                                                    String result = assets + ": " + task.getResult().size() + "" + "\n";
+                                                    textView.setText(result.toUpperCase());
+                                                }
+                                            }
+                                        });
                             }
-                        });
+                        }
+                    }
+                });
             }
         });
+
+//        get_info.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                final ArrayList<String> list = new ArrayList<>();
+//                db.collection("users").document(user_id).collection("laptop").whereEqualTo("department", editText.getText().toString()).get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    int count = 0;
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        String category_name = document.getData().get("category").toString();
+//                                        Log.d(TAG, "Here is the result: " + document.getId() + " => " + document.getData());
+////                                        textView.setText(document.getData().get("category").toString());
+//                                        list.add(document.getData().get("category").toString());
+//                                    }
+//                                    Set<String> unique = new HashSet<String>(list);
+//                                    for (String key : unique) {
+//                                        String result = (key + ": " + Collections.frequency(list, key));
+//                                        textView.append(result + "\n");
+//                                    }
+////                                    String result = ":" + list.size();
+////                                    textView.append(result);
+//                                } else {
+//                                    Log.d(TAG, "Error getting documents: ", task.getException());
+//                                }
+//
+//                            }
+//                        });
+//            }
+//        });
         return view;
 
     }
