984cd06ace7558d2c305c9cd30f23c55e650e20d
Prince <broughtmeup@gmail.com>
2019-03-22 21:03:24 +0530

    *Implemented User RecyclerView

--- app/src/main/java/com/prince/assetManagement/FragmentUser.java
+++ app/src/main/java/com/prince/assetManagement/FragmentUser.java
@@ -5,8 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,17 +17,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import static android.widget.LinearLayout.VERTICAL;
 
 
 public class FragmentUser extends Fragment {
@@ -34,25 +39,81 @@ public class FragmentUser extends Fragment {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
     private static final String TAG = "FragmentUser";
-
+    Spinner spinner;
+    ArrayList<String> mAssetType = new ArrayList<>();
+    ArrayList<String> mAssetNumber = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user, container, false);
+        Log.d(TAG, "initRecyclerView: init recycler view");
         get_info = view.findViewById(R.id.get_info);
-        editText = view.findViewById(R.id.asset_user);
-        textView = view.findViewById(R.id.result);
-        textView1 = view.findViewById(R.id.result_title);
+//        editText = view.findViewById(R.id.asset_user);
+//        textView = view.findViewById(R.id.result);
+//        textView1 = view.findViewById(R.id.result_title);
+        spinner = view.findViewById(R.id.spinner_user);
+
+        final List<String> username = new ArrayList<>();
+        final List<String> user_id = new ArrayList<>();
+
         assert getArguments() != null;
         final String admin_id = getArguments().getString("admin_id");
+        final String[] issued_to_id = new String[1];
+        Log.d(TAG, "onCreateView: user id " + issued_to_id[0]);
+
+        final RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        final GetInfoList adapter = new GetInfoList(mAssetType, mAssetNumber, getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        DividerItemDecoration decoration = new DividerItemDecoration(getActivity(), VERTICAL);
+        recyclerView.addItemDecoration(decoration);
+
+
+        db.collection("users")
+                .document(admin_id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot documentSnapshot = task.getResult();
+                            Map<String, String> users = (Map<String, String>) documentSnapshot.get("normal_users");
+                            for (Map.Entry<String, String> user : users.entrySet()) {
+                                Log.e(TAG, "onComplete: the user is: " + user.getKey() + " id is: " + user.getValue());
+                                username.add(user.getKey().toUpperCase());
+                                user_id.add(user.getValue());
+                            }
+                            ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, username);
+                            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            spinner.setAdapter(dataAdapter);
+                        }
+                    }
+                });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                issued_to_id[0] = user_id.get(i);
+                Log.d(TAG, "onItemSelected: user id is " + issued_to_id[0]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         get_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String issued_to = editText.getText().toString();
                 assert admin_id != null;
+                mAssetType.clear();
+                mAssetNumber.clear();
+                adapter.notifyDataSetChanged();
                 db.collection("users")
                         .document(admin_id)
                         .get()
@@ -68,7 +129,7 @@ public class FragmentUser extends Fragment {
                                         db.collection("users")
                                                 .document(admin_id)
                                                 .collection(assets.toString())
-                                                .whereEqualTo("issued_to", issued_to)
+                                                .whereEqualTo("issued_to_id", issued_to_id[0])
                                                 .get()
                                                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                                     @Override
@@ -78,20 +139,24 @@ public class FragmentUser extends Fragment {
                                                             Log.e(TAG, "onComplete: task is successful");
                                                             Log.e(TAG, "onComplete: executing" + task.getResult().getDocuments().toString());
                                                             Log.e(TAG, "onComplete: " + task.getResult().getMetadata().toString());
-                                                            textView1.setText(assets.toString().toUpperCase());
+//                                                            textView1.setText(assets.toString().toUpperCase());
                                                             String result = "Quantity: " + task.getResult().size() + "\n";
-                                                            textView.setText(result);
-                                                            textView.append("Date Issued: \n");
+                                                            String number = String.valueOf(task.getResult().size());
+//                                                            textView.setText(result);
+//                                                            textView.append("Date Issued: \n");
+                                                            mAssetType.add(assets.toString().toUpperCase());
+                                                            mAssetNumber.add(number);
+                                                            adapter.notifyDataSetChanged();
                                                             int count = 0;
-                                                            for (QueryDocumentSnapshot documentSnapshot1 : task.getResult()) {
-                                                                list.add(documentSnapshot1.getData().get("issued_date").toString());
-                                                                Log.e(TAG, "onComplete: document snapshot " + documentSnapshot1.getData().get("issued_date").toString());
-                                                            }
-                                                            Set<String> unique = new HashSet<String>(list);
-                                                            for (String key : unique) {
-                                                                String new_result = (Collections.frequency(list, key) + " on " + key);
-                                                                textView.append("   \u2022 " + new_result + "\n");
-                                                            }
+//                                                            for (QueryDocumentSnapshot documentSnapshot1 : task.getResult()) {
+//                                                                list.add(documentSnapshot1.getData().get("issued_date").toString());
+//                                                                Log.e(TAG, "onComplete: document snapshot " + documentSnapshot1.getData().get("issued_date").toString());
+//                                                            }
+//                                                            Set<String> unique = new HashSet<>(list);
+//                                                            for (String key : unique) {
+//                                                                String new_result = (Collections.frequency(list, key) + " on " + key);
+//                                                                textView.append("   \u2022 " + new_result + "\n");
+//                                                            }
                                                         }
                                                     }
                                                 });
